3dab07889984a6310613654d0402088b75bd02b6
Brian Waldon <bcwaldon@gmail.com>
2013-06-22 20:03:30 -0700

    Bootstrap

---
+++ changeling_client/__init__.py
@@ -0,0 +1,3 @@
+from changeling_client import metadata
+
+__version__ = metadata.VERSION
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+import changeling_client.metadata
+
+
+def parse_requirements():
+    return open('requirements.txt').readlines()
+
+
+setuptools.setup(
+    name='changeling-client',
+    version=changeling_client.metadata.VERSION,
+    packages=['changeling_client'],
+    install_requires=parse_requirements(),
+    entry_points={
+        'console_scripts': [
+            'changeling = changeling_client.cli:main',
+        ],
+    },
+)
