a907afd84821b2e26a381c7887815a7953a641ff
Brian Waldon <bcwaldon@gmail.com>
2013-06-22 20:37:53 -0700

    Use warlock for models

--- changeling_client/api.py
+++ changeling_client/api.py
@@ -1,18 +1,33 @@
 import urlparse
 import requests
 
-#import warlock
+import warlock
 
 
 class Service(object):
     def __init__(self, endpoint):
         self.endpoint = endpoint
-        self._connection = None
+        self._model = None
 
     def _request(self, method, rel_path):
         full_path = urlparse.urljoin(self.endpoint, rel_path)
         return requests.request(method, full_path)
 
+    @property
+    def model(self):
+        if self._model is None:
+            schema = self.get_schema()
+            self._model = warlock.model_factory(schema)
+        return self._model
+
+    def get_schema(self):
+        request = self._request('GET', '/schemas/change')
+        return request.json()
+
     def list_changes(self):
         request = self._request('GET', '/changes')
-        return request.json()
+        return [self.model(change) for change in request.json()]
+
+    def get_change(self, change_id):
+        request = self._request('GET', '/changes/%s' % change_id)
+        return self.model(request.json())
--- changeling_client/cli.py
+++ changeling_client/cli.py
@@ -10,4 +10,4 @@ parser.add_argument('--endpoint', required=True)
 def main():
     args = parser.parse_args()
     service = changeling_client.api.Service(args.endpoint)
-    print service.list_changes()
+    print service.get_change('43af1ff3-10b4-4060-9562-133d56efcc91')
